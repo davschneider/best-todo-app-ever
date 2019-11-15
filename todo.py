8522e62e5355280abff93a9aa3bad67fffefbf6e
David Schneider <davidschneider@Davids-MacBook-Pro.local>
2019-11-15 15:59:08 -0500

    adds the use of dictionaries to the todo app

--- todo.py
+++ todo.py
@@ -1,21 +1,62 @@
 
-def print_todos(todos):
-    if len(todos) == 0:
+pending_list = []
+completed_list = []
+
+def print_todos(pending_list, completed_list):
+    pending_menu_top = """
+====== Pending ======
+    """
+    print(pending_menu_top)
+    if len(pending_list) == 0:
         print("You have nothing to do.")
     else:
         i = 0
-        for todo in todos:
+        for todo in pending_list:
             print(f"{i}: {todo}")
             i += 1
+    pending_menu_bottom = """
+=====================    
+    """
+    print(pending_menu_bottom)
+    
+    completed_menu_top = """
+====== Completed ======
+    """
+    print(completed_menu_top)
 
-def add_todo(todos, item):
-    todos.append(item)
+    i = 0
+    for todos in completed_list:
+        print(f"{i}: {todo}")
+        i += 1
 
-def delete_todo(todos, index):
-    try:
-        del todos[index]
-    except IndexError:
-        print("That todo does not exist.")
+    completed_menu_bottom = """
+=======================    
+    """
+    print(completed_menu_bottom)
+
+def add_todo(lst, item):
+    new_dict = {}
+    new_dict[item] = 'do the thing'
+    new_dict['completed'] = False
+    
+    lst.append(new_dict)
+    
+
+def complete_todo(todos, index):
+    todos[index]['completed'] = True
+    completed_todo = todos[index]
+    # print(f"{completed_todo} is completed.")
+    completed_list.append(completed_todo)
+    
+    del todos[index]
+
+    
+    # completed_list.append(completed_todo)
+    
+    # try:
+    #     del todos[index]
+    # except IndexError:
+    #     print("That todo does not exist.")
 
 def print_menu():
     message = """
@@ -40,9 +81,7 @@ def get_choice(prompt="Choose one: "):
     return choice
     
 def main():
-    todo_list = []
-
-    
+    # todo_list = []
     is_running = True
     while is_running:
         print_menu()
@@ -51,13 +90,15 @@ def main():
             print("K. Byeeee!")
             is_running = False
         elif choice == 1:
-            print_todos(todo_list)
+            print_todos(pending_list, completed_list)
         elif choice == 2:
             new_todo = input("Enter a todo: ")
-            add_todo(todo_list, new_todo)
+            add_todo(pending_list, new_todo)
+            # print(pending_list)
         elif choice == 3:            
             index_to_delete = get_choice("Enter the index to complete: ")
-            delete_todo(todo_list, index_to_delete)
+            complete_todo(pending_list, index_to_delete)
+            # print(completed_list)
+    
         
-
 main()
